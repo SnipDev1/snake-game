9659767e8f22cb7664324823922a35a22f19b123
Dmitry <111853324+SnipDev1@users.noreply.github.com>
2023-04-27 21:13:58 +0700

    Update main.py
    I have added a system, which provide you to change resolution, without adding a modifications into code.

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import sys
 import threading
 import time
 
+import art
 import keyboard
 
-game_field = [[], [], [], [], [],
-              [], [], [], [], [],
-              [], [], [], [], [],
-              [], [], [], [], [],
-              [], [], [], [], []]
+width = 10
+height = 10
+game_field = [[] for _ in range(width * height)]
 closer = False
 IsActive = False
 time_for_render = 0.1
@@ -19,10 +18,13 @@ x_snake = 0
 y_snake = 0
 direction = ""
 index_previous = 0
-coin = 0
+score = 0
 symbol_player = "📀"
 symbol_coin = "😡"
 symbol_background = "🕗"
+direction_prev = ''
+IsRestart = False
+IsQuit = False
 
 
 class Graphic:
@@ -33,13 +35,13 @@ class Graphic:
             time.sleep(time_for_render)
             os.system("cls")
             x = 0
-            y = 5
+            y = width
 
-            for i in range(5):
+            for i in range(height):
                 print(Graphic.cleaning(str(game_field[x:y])) + "\n")
-                x += 5
-                y += 5
-
+                x += width
+                y += width
+            art.tprint('Score: {}'.format(score))
             if closer:
                 return 0
 
@@ -47,24 +49,24 @@ class Graphic:
 
         symbols_to_remove = ["'", ",", "[", "]"]
         for symbol in symbols_to_remove:
-            self = self.replace(symbol, ' ')
+            self = self.replace(symbol, '')
         return self.strip()
 
     @staticmethod
     def start():
         i = 0
         global closer
-        while i < 25:
+        while i < width * height:
             game_field[i] = symbol_background
             i += 1
 
 
 class GameLogic:
-    def convert_coordinates_to_index(x, y, width):
+    def convert_coordinates_to_index(x, y):
         return y * width + x
 
     def moving():
-        global x_snake, y_snake, index_previous, direction, coin, IsActive, snake_length, closer
+        global x_snake, y_snake, index_previous, direction, score, IsActive, snake_length, closer
         y_snake = 0
         x_snake = 0
 
@@ -75,18 +77,18 @@ class GameLogic:
 
             time.sleep(time_for_render)
             time.sleep(0.2)
-            if direction == "Down" and y_snake <= 5:
+            if direction == "Down" and y_snake <= height:
                 x_snake_previous.append(x_snake)
                 y_snake_previous.append(y_snake)
                 y_snake += 1
                 if step > 1:
                     index_previous = GameLogic.convert_coordinates_to_index(x_snake_previous[step - snake_length],
-                                                                            y_snake_previous[step - snake_length], 5)
-                if y_snake == 5:
+                                                                            y_snake_previous[step - snake_length])
+                if y_snake == height:
                     y_snake = 0
-                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake, 5)
+                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake)
                 if game_field[index] == symbol_coin:
-                    coin += 1
+                    score += 1
                     snake_length += 1
                     IsActive = False
                 if game_field[index] == symbol_player:
@@ -101,49 +103,49 @@ class GameLogic:
                 y_snake -= 1
 
                 if y_snake == -1:
-                    y_snake = 4
+                    y_snake = height - 1
                 if step > 1:
                     index_previous = GameLogic.convert_coordinates_to_index(x_snake_previous[step - 1],
-                                                                            y_snake_previous[step - 1], 5)
-                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake, 5)
+                                                                            y_snake_previous[step - 1])
+                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake)
                 if game_field[index] == symbol_coin:
-                    coin += 1
+                    score += 1
                     IsActive = False
                     snake_length += 1
                 if game_field[index] == symbol_player:
                     closer = True
                 game_field[index] = symbol_player
 
-            if direction == "Right" and x_snake < 5:
+            if direction == "Right" and x_snake < width:
 
                 x_snake_previous.append(x_snake)
                 y_snake_previous.append(y_snake)
                 x_snake += 1
 
-                if x_snake == 5:
+                if x_snake == width:
                     x_snake = 0
 
-                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake, 5)
+                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake)
                 if game_field[index] == symbol_coin:
-                    coin += 1
+                    score += 1
                     IsActive = False
                     snake_length += 1
                 if game_field[index] == symbol_player:
                     closer = True
                 game_field[index] = symbol_player
 
-            if direction == "Left" and x_snake < 5:
+            if direction == "Left" and x_snake < width:
 
                 x_snake_previous.append(x_snake)
                 y_snake_previous.append(y_snake)
                 x_snake -= 1
 
                 if x_snake == -1:
-                    x_snake = 4
+                    x_snake = width - 1
 
-                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake, 5)
+                index = GameLogic.convert_coordinates_to_index(x_snake, y_snake)
                 if game_field[index] == symbol_coin:
-                    coin += 1
+                    score += 1
                     IsActive = False
                     snake_length += 1
                 if game_field[index] == symbol_player:
@@ -154,7 +156,7 @@ class GameLogic:
                 step += 1
                 if step > 1:
                     index_previous = GameLogic.convert_coordinates_to_index(x_snake_previous[step - snake_length],
-                                                                            y_snake_previous[step - snake_length], 5)
+                                                                            y_snake_previous[step - snake_length])
                 game_field[index_previous] = symbol_background
             if closer:
                 return 0
@@ -163,9 +165,9 @@ class GameLogic:
         global IsActive
 
         if caused and not IsActive:
-            x_coin = random.randint(0, 4)
-            y_coin = random.randint(0, 4)
-            index = GameLogic.convert_coordinates_to_index(x_coin, y_coin, 5)
+            x_coin = random.randint(0, width - 1)
+            y_coin = random.randint(0, height - 1)
+            index = GameLogic.convert_coordinates_to_index(x_coin, y_coin)
             if game_field[index] == symbol_background:
                 game_field[index] = symbol_coin
                 IsActive = True
@@ -192,18 +194,33 @@ class Binds:
 
         sys.exit("Exit")
 
+    def restart(self):
+        global IsRestart, IsQuit
+        if self == 'y':
+            IsRestart = True
+        elif self == 'n':
+            IsQuit = True
+
     def change_direction(self):
-        global direction
+        global direction, direction_prev
 
         match self:
             case "w":
-                direction = "Up"
+                direction_prev = "Up"
+                if direction != "Down":
+                    direction = direction_prev
             case "a":
-                direction = "Left"
+                direction_prev = "Left"
+                if direction != "Right":
+                    direction = direction_prev
             case "s":
-                direction = "Down"
+                direction_prev = "Down"
+                if direction != "Up":
+                    direction = direction_prev
             case "d":
-                direction = "Right"
+                direction_prev = "Right"
+                if direction != "Left":
+                    direction = direction_prev
 
 
 def pressed_button():
@@ -213,6 +230,8 @@ def pressed_button():
     keyboard.add_hotkey('a', lambda: Binds.change_direction("a"))
     keyboard.add_hotkey('s', lambda: Binds.change_direction("s"))
     keyboard.add_hotkey('d', lambda: Binds.change_direction("d"))
+    keyboard.add_hotkey('y', lambda: Binds.restart('y'))
+    keyboard.add_hotkey('n', lambda: Binds.restart('n'))
 
 
 def main():
